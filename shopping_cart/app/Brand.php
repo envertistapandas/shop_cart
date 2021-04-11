8cceaf442f4384f3084772a722d055a0756f3ca1
Tapan Das <tapandas025@gmail.com>
2021-04-12 00:45:54 +0530

    paramita_di_shopping_cart_project
    Today updated 12-04-2021

---
+++ shopping_cart/app/Brand.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+class Brand extends Model
+{
+    protected $table = 'brands';
+    use SoftDeletes;
+
+    public function product()
+    {
+        return $this->hasOne('App\Product');
+    }
+}
---
+++ shopping_cart/resources/views/welcome.blade.php
@@ -0,0 +1,224 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Shopping_cart</title>
+    <link href="{{asset('frontend/css/bootstrap.min.css')}}" rel="stylesheet">
+    <link href="{{asset('frontend/css/font-awesome.min.css')}}" rel="stylesheet">
+    <link href="{{asset('frontend/css/prettyPhoto.css')}}" rel="stylesheet">
+    <link href="{{asset('frontend/css/price-range.css')}}" rel="stylesheet">
+    <link href="{{asset('frontend/css/animate.css')}}" rel="stylesheet">
+    <link href="{{asset('frontend/css/main.css')}}" rel="stylesheet">
+    <link href="{{asset('frontend/css/responsive.css')}}" rel="stylesheet">
+    <!--[if lt IE 9]>
+    <script src="js/html5shiv.js"></script>
+    <script src="js/respond.min.js"></script>
+    <![endif]-->       
+    <link rel="shortcut icon" href="{{URL::to('frontend/images/ico/favicon.ico')}}">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="{{URL::to('frontend/images/ico/apple-touch-icon-144-precomposed.png')}}">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="{{URL::to('frontend/images/ico/apple-touch-icon-114-precomposed.png')}}">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="{{URL::to('frontend/images/ico/apple-touch-icon-72-precomposed.png')}}">
+    <link rel="apple-touch-icon-precomposed" href="{{URL::to('frontend/images/ico/apple-touch-icon-57-precomposed.png')}}">
+</head><!--/head-->
+
+<body>
+<div class="flex-center position-ref full-height">
+            @if (Route::has('login'))
+                <div class="top-right links">
+                    @auth
+                        <a href="{{ url('/home') }}">Home</a>
+                    @else
+                        <a href="{{ route('login') }}">Login</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}">Register</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif  
+</div>
+    <header id="header"><!--header-->
+        <div class="header_top"><!--header_top-->
+            
+        </div><!--/header_top-->
+        
+        <div class="header-middle"><!--header-middle-->
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-4">
+                        <div class="logo pull-left">
+                            <!--<a href="index.html"><img src="{{URL::to('frontend/images/home/logo.png')}}" alt="" /></a>-->
+                            <h2>Shopping Cart</h2>
+                        </div>
+                    </div>
+                    <div class="col-sm-8">
+                        <div class="shop-menu pull-right">
+                            <ul class="nav navbar-nav">
+                                <!--<li><a href="#"><i class="fa fa-user"></i> Account</a></li>
+                                <li><a href="#"><i class="fa fa-star"></i> Wishlist</a></li>
+                                <li><a href="checkout.html"><i class="fa fa-crosshairs"></i> Checkout</a></li>
+                                <li><a href="cart.html"><i class="fa fa-shopping-cart"></i> Cart</a></li>
+                                <li><a href="login.html"><i class="fa fa-lock"></i> Login</a></li>-->
+                                 @if (Route::has('login'))
+                                     <div class="top-right links">
+                                        @auth
+                                             <a href="{{ url('/home') }}">Home</a>
+                                @else
+                                            <a href="{{ route('login') }}">Login</a>
+
+                                            @if (Route::has('register'))
+                                             <a href="{{ route('register') }}">Register</a>
+                                            @endif
+                                         @endauth
+                                    </div>
+                                @endif  
+
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div><!--/header-middle-->
+    
+        <div class="header-bottom"><!--header-bottom-->
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-9">
+                        <div class="navbar-header">
+                            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                                <span class="sr-only">Toggle navigation</span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                            </button>
+                        </div>
+                        <div class="mainmenu pull-left">
+                            <ul class="nav navbar-nav collapse navbar-collapse">
+                                <li><a href="index.html" class="active">Home</a></li>
+                                <li class="dropdown"><a href="#">Shop<i class="fa fa-angle-down"></i></a>
+                                    <ul role="menu" class="sub-menu">
+                                        <li><a href="shop.html">Products</a></li>
+                                        <li><a href="product-details.html">Product Details</a></li> 
+                                        <li><a href="checkout.html">Checkout</a></li> 
+                                        <li><a href="cart.html">Cart</a></li> 
+                                        <li><a href="login.html">Login</a></li> 
+                                    </ul>
+                                </li> 
+                               
+                                
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-sm-3">
+                        <div class="search_box pull-right">
+                            <input type="text" placeholder="Search"/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div><!--/header-bottom-->
+    </header><!--/header-->
+
+
+    
+    <section>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-3">
+                    <div class="left-sidebar">
+                        <h2>Category</h2>
+                        <div class="panel-group category-products" id="accordian"><!--category-productsr-->
+                            <div class="panel panel-default">
+                           <?php 
+                                $all_published_category=DB::table('categorys')
+                                                            ->where('publication_status',1)
+                                                            ->get();
+                                foreach ($all_published_category as $v_category) 
+                                {
+                            ?>                                                          
+                         
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <h4 class="panel-title"><a href="{{URL::to('/product_by_category'.$v_category->id)}}">{{$v_category->category_name}}</a></h4>
+                                </div>
+                            </div>
+                        <?php  
+                        }
+                        ?>
+                        </div><!--/category-products-->
+                       </div>
+                        <div class="brands_products"><!--brands_products-->
+                            <h2>Brands</h2>
+                            <div class="brands-name">
+                                <ul class="nav nav-pills nav-stacked">
+                            <?php 
+                                $all_published_brand=DB::table('brands')
+                                                            ->where('brand_status',1)
+                                                            ->get();
+                                foreach ($all_published_brand as $v_brand) 
+                                { 
+                            ?>
+                                  <li><a href="{{URL::to('/product_by_brand'.$v_brand->id)}}"> <span class="pull-right"></span>{{$v_brand->brand_name}}</a></li>  
+                             
+                              <?php 
+                                   }
+                              ?>  
+                                   
+                                </ul>
+                            </div>
+                        </div><!--/brands_products-->
+                        
+                        
+                        
+                        
+                    
+                    </div>
+                </div>
+
+                 <div class="col-sm-9 padding-right">
+                    <div class="features_items"><!--features_items-->
+                        @yield('content');
+                    </div><!--features_items-->
+                 </div>
+
+            </div>
+        </div>
+    </section>
+    
+    <footer id="footer"><!--Footer-->
+        <div class="footer-bottom">
+            <div class="container">
+                <div class="row">
+                    <p class="pull-left">Copyright © 2013 E-SHOPPER Inc. All rights reserved.</p>
+                    <p class="pull-right">Designed by <span><a target="_blank" href="http://www.themeum.com">Themeum</a></span></p>
+                </div>
+            </div>
+        </div>
+        
+    </footer><!--/Footer-->
+    
+
+  
+    <script src="{{asset('frontend/js/jquery.js')}}"></script>
+    <script src="{{asset('frontend/js/bootstrap.min.js')}}"></script>
+    <script src="{{asset('frontend/js/jquery.scrollUp.min.js')}}"></script>
+    <script src="{{asset('frontend/js/price-range.js')}}"></script>
+    <script src="{{asset('frontend/js/jquery.prettyPhoto.js')}}"></script>
+    <script src="{{asset('frontend/js/main.js')}}"></script>
+    
+    <!-- bootbox code -->
+    <script type="text/javascript" src="{{asset('https://raw.githubusercontent.com/makeusabrew/bootbox/gh-pages/bootbox.js')}}"></script>
+    <script>
+    $(document).on("click", ".remove", function(e) {
+        bootbox.confirm("Are you sure you want to delete?", function(result) {
+            if(result){
+              console.log('write code of remove item.');
+            }
+        }); 
+    });
+</script>
+</body>
+</html>
